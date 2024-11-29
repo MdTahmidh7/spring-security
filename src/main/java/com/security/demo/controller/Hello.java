6e63ccb77c7db4535f2706d95784b3f8e223a636
PrinceFT <ftprince908@gmail.com>
2024-11-29 22:51:55 +0600

    Authentication from database functionality added.

--- src/main/java/com/security/demo/controller/Hello.java
+++ src/main/java/com/security/demo/controller/Hello.java
@@ -1,14 +1,17 @@
 package com.security.demo.controller;
 
+import com.security.demo.entity.Users;
+import com.security.demo.repo.UserRepo;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Hello {
+
+    @Autowired
+    private UserRepo userRepo;
 
 
     @GetMapping("")
@@ -25,5 +28,10 @@ public class Hello {
     @PostMapping("/post-hello")
     public String postHello(@RequestBody String message){
         return message;
+    }
+
+    @GetMapping("/public")
+    public Users getCSRFToken(@RequestParam Integer id){
+        return userRepo.findById(id).get();
     }
 }
---
+++ src/main/java/com/security/demo/repo/UserRepo.java
@@ -0,0 +1,12 @@
+package com.security.demo.repo;
+
+import com.security.demo.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<Users,Integer> {
+
+    Users findByUsername(String username);
+
+}
