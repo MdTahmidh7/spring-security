220d7f7892b8bb2140e208aeec66cdb82997da49
MdTahmidh7 <mdtahmidh7@gmail.com>
2024-11-29 17:57:44 +0600

    Line removed

--- src/main/java/com/security/demo/config/SecurityConfig.java
+++ src/main/java/com/security/demo/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.security.demo.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
